026b145acf471f86ba33dd058eb8bcd068f44f3e
einarhjorleifsson <einar.hjorleifsson@gmail.com>
2015-06-27 11:24:34 +0000

    add title and ref to reglugerd*.csv

---
+++ R/data_misc.R
@@ -0,0 +1,11 @@
+#' bormicon
+#'
+#' Bormicon areas
+#' 
+#' Modified object from the \code{fjolst}-package
+#'
+#' @format \code{SpatialPolygonsDataFrame}
+#'
+#' @author Höskuldur Björnsson <hoski@@hafro.is>
+#' 
+"bormicon"
