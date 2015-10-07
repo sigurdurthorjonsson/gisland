2bd2b487123ffa3d8c2867cead488884541f0eee
einarhjorleifsson <einar.hjorleifsson@gmail.com>
2015-10-07 12:21:58 +0000

    geo_convert add inverse

--- R/geofunctions.R
+++ R/geofunctions.R
@@ -13,8 +13,11 @@
 #' @author Hoskuldur Bjornsson <hoski@@hafro.is>
 #' 
 #' @param x Vector of the form DDMMSS (degree-minutes-seconds)
-geo_convert <- function (x)
+#' @param inverse Which conversion should be undertaken, default from 
+#' degrees-minutes-decimal minutes (DDMMmm) to decimal degrees (DD.dd)
+geo_convert <- function (x, inverse = FALSE)
 {
+  if(!inverse) {
   i <- sign(x)
   x <- abs(x)
   # x1 <- x%%10000
@@ -24,6 +27,14 @@ geo_convert <- function (x)
   #   print(paste("error > 60 min nr", k, x[k]))
   min <- (x/100) - trunc(x/10000) * 100
   return((i * (x + (200/3) * min))/10000)
+  } else {
+    i <- sign(x)
+    x <- abs(x)
+    p1 <- floor(x)
+    p2 <- floor((x - p1) * 60)
+    p3 <- round((x - p1 - p2/60) * 100 * 60)
+    return(i * (p1 * 10000 + p2 * 100 + p3))
+  }
 }
 
 
