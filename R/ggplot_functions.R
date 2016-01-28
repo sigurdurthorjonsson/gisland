5e60151e6b7f2058d1d5aef59bb4b3f0b779c520
Einar Hjorleifsson <einar.hjorleifsson@gmail.com>
2016-01-28 16:07:42 +0000

    scale_longitude and scale_latitude fix

--- R/ggplot_functions.R
+++ R/ggplot_functions.R
@@ -2,13 +2,13 @@
 #'
 #' @param from XXX
 #' @param to XXX
-#' @param step XXX
+#' @param by XXX
 #' @param ... XXX
 #'
 #' @export
 
-scale_longitude <- function(from=-180, to=180, step=1, ...) {
-  x <- breaks(from, to, step)
+scale_longitude <- function(from=-180, to=180, by=1, ...) {
+  x <- breaks(from, to, by)
   x$txt <- ifelse(x$Sign < 0, paste(x$txt,"W"), paste(x$txt,"E"))
   return(scale_x_continuous(breaks = x$value, labels = x$txt, expand = c(0, 0), ...))
 }
@@ -16,15 +16,15 @@ scale_longitude <- function(from=-180, to=180, step=1, ...) {
 
 #' Title
 #'
-#' @param ymin XXX
-#' @param ymax XXX
-#' @param step XXX
+#' @param from XXX
+#' @param to XXX
+#' @param by XXX
 #' @param ... XXX
 #'
 #' @export
 #'
-scale_latitude <- function(ymin=-90, ymax=90, step=0.5, ...) {
-  x <- breaks(from, to, step)
+scale_latitude <- function(from=-90, to=90, by=0.5, ...) {
+  x <- breaks(from, to, by)
   x$txt <- ifelse(x$Sign < 0, paste(x$txt,"S"), paste(x$txt,"N"))
   return(scale_y_continuous(breaks = x$value, labels = x$txt, expand = c(0, 0), ...))
 }
@@ -35,7 +35,8 @@ breaks <- function(from = -180, to = 180, step = 1) {
   dd <- trunc(x)
   j <- length(unique(dd)) != length(dd)
   dd <- abs(dd)
-  txt <- paste(dd, "°", sep = "")
+  #sprintf("%X", as.integer(charToRaw("°")))
+  txt <- paste(dd, "\u00B0", sep = "")
   mm <- x%%1
   mm <- round(mm * 60, 2)
   i <- mm > 0
