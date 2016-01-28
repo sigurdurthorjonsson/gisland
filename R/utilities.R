e75755e97eedcecd32dc0bff24fdc626d3430ce6
Einar Hjorleifsson <einar.hjorleifsson@gmail.com>
2016-01-28 16:11:07 +0000

    r2lat, r2lon added

--- R/utilities.R
+++ R/utilities.R
@@ -1,3 +1,30 @@
+#' @title Returns the lon for a statistical square (tilkynningaskildureitur)
+#'
+#' @description A dplyr-friendly variant of the geo::r2d, returning only a
+#' vector of longitudes. To be used inside mutate.
+#'
+#' @export
+#'
+#' @param x Numerical vector containing statistical square
+r2lon <-  function(x) {
+  -((x-floor(x/100)*100)%%50 + 0.5)
+}
+
+#' @title Returns the latitude for a statistical square (tilkynningaskildureitur)
+#'
+#' @description A dplyr-friendly variant of the geo::r2d, returning only a
+#' vector of latitudes. To be used inside mutate.
+#'
+#' @export
+#'
+#' @param x Numerical vector containing statistical square
+r2lat <- function(x) {
+  lat <- floor(x/100)
+  lon <- (x - lat * 100)%%50
+  halfb <- (x - 100 * lat - lon)/100
+  lat <- lat + 60 + halfb + 0.25
+}
+
 #' degrees
 #' 
 #' Af function that adds the suffix degree to object. Used e.g.
